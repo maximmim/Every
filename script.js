ed8d01cb1366f3fc501aed026fd342c7f809d4ce
maximmim <93773088+maximmim@users.noreply.github.com>
2022-03-22 13:23:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function push() {
+if (!"Notification" in window) {
+    alert("Ваш браузер не подерживает увидомления :(")
+}
+if (Notification.permission === "granted") {
+    setTimeout(notu,2000)
+}
+else if (Notification.permission !== "denied") {
+    Notification.requestPermission( function (permission) {
+        if (!("permission" in Notification)) {
+            Notification.permission = permission;
+            if (permission === "granted") {
+                setTimeout(notu,2000)
+            }
+        }
+    })
+}
+}
+function notu() {
+let notif = new Notification ("Проведа Every у новий пост", {
+    tag : "ache-mail",
+    body: "Every Просе вас відпочити",
+    icon: "icon/da.jpg"
+})
+
+}
