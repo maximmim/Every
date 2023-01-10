3589a8bf2d96d36867569a692e50499bc3890bf0
maximmim <93773088+maximmim@users.noreply.github.com>
2023-01-10 07:40:58 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -169,8 +169,229 @@ function getRandomArrayElement(arr){
 
  }
 
- function home() {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function lox() {
+
+}
+function loxcs() 
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function skip() {
+    document.getElementById("home").style.display = "none"
+    document.getElementById("close").style.display = "block"
+    document.getElementById("timg2").style.display = "block"
+    document.getElementById("tbutg2").style.display = "block"
+    document.getElementById("timg1").style.display = "none"
+    document.getElementById("timg2").style.display = "block"
+}
+
+function skip1() {
+    document.getElementById("home").style.display = "none"
+    document.getElementById("close").style.display = "block"
+    document.getElementById("add").style.display = "none"
+    document.getElementById("timg2").style.display = "block"
+    document.getElementById("tbutgw1").style.display = "none"
+    document.getElementById("tbutg3").style.display = "block"
+    document.getElementById("timg1").style.display = "none"
+}
+function skip2() {
+    document.getElementById("home").style.display = "none"
+    document.getElementById("close").style.display = "none"
+    document.getElementById("add").style.display = "block"
+    document.getElementById("timg3").style.display = "block"
+    document.getElementById("timg2").style.display = "none"
+    document.getElementById("tbutg4").style.display = "block"
+    document.getElementById("tbutg3").style.display = "none"
+}
+function skip3() {
+    document.getElementById("home").style.display = "none"
+    document.getElementById("close").style.display = "none"
+    document.getElementById("add").style.display = "none"
+    document.getElementById("timg3").style.display = "none"
+    document.getElementById("timg2").style.display = "none"
+    document.getElementById("tbutg4").style.display = "none"
+    document.getElementById("tbutg3").style.display = "none"
+    document.getElementById("struktur1").style.display = "block"
+    document.getElementById("struktur1").style.width = "100px"
+    document.getElementById("struktur1").style.height = "100px"
+    document.getElementById("timg4").style.display = "block"
+    document.getElementById("tbutg4").style.display = "none"
+    document.getElementById("tbutg3").style.display = "none"
+    document.getElementById("tbutg2").style.display = "none"
+    document.getElementById("tbutg1").style.display = "none"
+    
+}
+
+
+
+
+function og() {
+    document.getElementById("home").style.display = "none"
+    document.getElementById("close").style.display = "none"
+    document.getElementById("add").style.display = "none"
+    document.getElementById("timg3").style.display = "none"
+    document.getElementById("timg2").style.display = "none"
+    document.getElementById("tbutg4").style.display = "none"
+    document.getElementById("tbutg3").style.display = "none"
+    document.getElementById("struktur1").style.display = "none"
+    document.getElementById("struktur1").style.width = "100px"
+    document.getElementById("struktur1").style.height = "100px"
+    document.getElementById("timg4").style.display = "none"
+    document.getElementById("tbutg4").style.display = "none"
+    document.getElementById("tbutg3").style.display = "none"
+    document.getElementById("tbutg2").style.display = "none"
+    document.getElementById("tbutg1").style.display = "none"
+    document.getElementById("akui").style.display = "block"
+    document.getElementById("ttbut1").style.display = "block"
+    document.getElementById("ttbut2").style.display = "block"
+
+}
+
+function ski() {
+    document.getElementById("ttbut1").style.display = "none"
+document.getElementById("ttbut2").style.display = "none"
+document.getElementById("akui").style.display = "none"
+document.getElementById("register").style.display = "block"
+document.getElementById("daw1").style.display = "block"
+document.getElementById("regbut1").style.display = "block"
+
+document.getElementById("home").style.display = "none"
+document.getElementById("timg1").style.display = "none"
+document.getElementById("tbutg1").style.display = "none"
+document.getElementById("tbutgw1").style.display = "none"
+document.getElementById("tbutg2").style.display = "none"
+}
+
+function skibyes() {
+    document.getElementById("tbutgw1").style.display = "block"
+    document.getElementById("home").style.display = "block"
+document.getElementById("struktur1").style.display = "none"
+document.getElementById("struktur2").style.display = "none"
+document.getElementById("ttext1").style.display = "none"
+document.getElementById("ttext1x1").style.display = "none"
+document.getElementById("tbut1").style.display = "none"
+document.getElementById("tbut2").style.display = "none"
+document.getElementById("timg1").style.display = "block"
+document.getElementById("timg2").style.display = "none"
+
+document.getElementById("tbutg1").style.display = "block"
+document.getElementById("tbutg2").style.display = "block"
+document.getElementById("tbutg1").style.display = "block"
+}
+
+function tskibyes() {
+    document.getElementById("home").style.display = "none"
+document.getElementById("struktur1").style.display = "none"
+document.getElementById("struktur2").style.display = "none"
+document.getElementById("dwa").style.display = "block"
+document.getElementById("add").style.display = "none"
+document.getElementById("close").style.display = "none"
+document.getElementById("te").style.display = "block"
+document.getElementById("tutor").style.display = "none"
+}
+
+
+function tskibno() {
+document.getElementById("ttbut1").style.display = "none"
+document.getElementById("ttbut2").style.display = "none"
+document.getElementById("akui").style.display = "none"
+document.getElementById("register").style.display = "block"
+document.getElementById("daw1").style.display = "block"
+document.getElementById("regbut1").style.display = "block"
+}
+
+function skibno() {
+    document.getElementById("home").style.display = "none"
+document.getElementById("struktur1").style.display = "none"
+document.getElementById("struktur2").style.display = "none"
+document.getElementById("dwa").style.display = "block"
+document.getElementById("add").style.display = "none"
+document.getElementById("close").style.display = "none"
+document.getElementById("te").style.display = "block"
+document.getElementById("tutor").style.display = "none"
+}
+
+function Eror_network() {
+    document.getElementById("home").style.display = "none"
+    document.getElementById("struktur1").style.display = "none"
+    document.getElementById("struktur2").style.display = "none"
+    document.getElementById("Ero").style.display = "none"
+    document.getElementById("add").style.display = "none"
+    document.getElementById("close").style.display = "none"
+
+    document.getElementById("tutor").style.display = "none"    
+    document.getElementById('Eror').style.display = 'block';
+
+}
+
+
+function checkOnlineState(){
+    if (navigator.onLine){
+      console.log("confirm")
+    } else {
+    //alert('Вибачте в вас немає зйєднаня з інтернетом :('); 
+    document.location.replace("Eror.html")
+    }
+  }
+  window.addEventListener('online',  checkOnlineState);
+  
+
+setInterval(checkOnlineState,10)
+
+
+ 
+function home() {
+    if (localStorage.getItem('daw') == "true"){
+        document.location.replace("/users/homeg.html")
+    }
+    else{ 
     if (localStorage.dw == "Анюта"){
         p = 1;
     }
@@ -179,7 +400,7 @@ function getRandomArrayElement(arr){
         p = 2;
     }
     else if (localStorage.dw == "Vika"){
-        p = 3;
+        p = 3; 
     }
     else if (localStorage.dw == "Artem") {
         p = 4;
@@ -188,13 +409,19 @@ function getRandomArrayElement(arr){
         p = -1;
     }
     if(p == -1) {
+
+
+        document.getElementById("tutor").style.display = "block"
+        
+/**/
 document.getElementById("home").style.display = "none"
 document.getElementById("struktur1").style.display = "none"
 document.getElementById("struktur2").style.display = "none"
-document.getElementById("dwa").style.display = "block"
+//document.getElementById("dwa").style.display = "block"
 document.getElementById("add").style.display = "none"
 document.getElementById("close").style.display = "none"
-document.getElementById("te").style.display = "block"
+//document.getElementById("te").style.display = "block"
+
     }
 
     if (p == 1){
@@ -212,23 +439,23 @@ document.getElementById("te").style.display = "block"
     }
 }
 
-
+}
 
 function cekinf() {
-    if (j == "https://memepedia.ru/wp-content/uploads/2019/01/hamster.jpg") {
+    if (j == "/icon/hamster.jpg") {
         document.location.replace("/users/home.html")
     }
-    if (jj == "https://memepedia.ru/wp-content/uploads/2019/01/hamster.jpg") {
+    if (jj == "/icon/hamster.jpg") {
         document.location.replace("/users/home.html")
     }
 
 
 
-    if (j === "https://i.gifer.com/7TMH.gif" )
+    if (j === "/icon/7TMH.webp" )
     {
         document.location.replace("/users/home.html")
     }
-    if (jj === "https://i.gifer.com/7TMH.gif") {
+    if (jj === "/icon/7TMH.webp") {
         document.location.replace("/users/home.html")
     }
 
@@ -243,10 +470,12 @@ function cekinf() {
     
 }
 
+
+
 function frandom() 
 {
-    memas = ["https://i.gifer.com/7TMH.gif","/icon/a.jpg","/icon/gg.png","/icon/dwa.webp","/icon/like.png","/icon/sticker.webp","/icon/images.jpg","https://memepedia.ru/wp-content/uploads/2019/01/hamster.jpg"]
-    memash = ["https://i.gifer.com/7TMH.gif","/icon/a.jpg","/icon/dwa.webp","/icon/sticker.webp","https://memepedia.ru/wp-content/uploads/2019/01/hamster.jpg"]
+    memas = ["/icon/7TMH.webp","/icon/a.jpg","/icon/gg.png","/icon/dwa.webp","/icon/like.png","/icon/sticker.webp","/icon/images.jpg","/icon/hamster.jpg"]
+    memash = ["/icon/7TMH.webp","/icon/a.jpg","/icon/dwa.webp","/icon/sticker.webp","/icon/hamster.jpg"]
     jj = getRandomArrayElement(memash)
     j = getRandomArrayElement(memas)    
     
@@ -275,8 +504,8 @@ function frandom()
     }
 }
 function random() {
-memas = ["https://i.gifer.com/F1oX.gif","https://i.gifer.com/19ps.gif","https://i.gifer.com/XKim.gif","https://i.gifer.com/y7.gif","https://i.gifer.com/7tDC.gif","https://i.gifer.com/4jcE.gif","https://i.gifer.com/VhdI.gif","https://i.gifer.com/PXkM.gif","https://i.gifer.com/7TMH.gif","/icon/a.jpg","/icon/gg.png","/icon/sticker.webp","https://memepedia.ru/wp-content/uploads/2019/01/hamster.jpg"]
-memash = ["https://i.gifer.com/7TMH.gif","/icon/a.jpg","/icon/dwa.webp","/icon/sticker.webp","https://memepedia.ru/wp-content/uploads/2019/01/hamster.jpg"]
+memas = ["/icon/7TMH.webp","https://i.gifer.com/19ps.gif","https://i.gifer.com/XKim.gif","https://i.gifer.com/y7.gif","https://i.gifer.com/7tDC.gif","https://i.gifer.com/4jcE.gif","https://i.gifer.com/VhdI.gif","https://i.gifer.com/PXkM.gif","https://i.gifer.com/7TMH.gif","/icon/a.jpg","/icon/gg.png","/icon/sticker.webp","/icon/hamster.jpg"]
+memash = ["/icon/7TMH.webp","/icon/a.jpg","/icon/dwa.webp","/icon/sticker.webp","/icon/hamster.jpg"]
 jj = getRandomArrayElement(memash)
 j = getRandomArrayElement(memas)
 document.getElementById("hed3").style.backgroundImage = "url("+j+")";
@@ -295,6 +524,8 @@ document.getElementById("st2hed2").style.backgroundImage = "url("+jj+")";
 
 
 
+
+
 str = ["str1","str2"]
 h = getRandomArrayElement(str)
 if (h == "str1") {
@@ -303,10 +534,28 @@ if (h == "str1") {
 if (h == "str2") {
     fstr2()
 }
+
 }
 
-
 setTimeout(random, 100)
+
+function maked() {
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 3; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  }
+
+
+
+console.log("https://i.gifer.com/"+maked()+".gif")
+
+
+
+//https://i.gifer.com/ovy.gif
 
 function makeid() {
     var text = "";
@@ -318,7 +567,15 @@ function makeid() {
     return text;
   }
 
-
+  function makei() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 4; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  }
 
 function ceking() 
 
@@ -328,7 +585,7 @@ var id = makeid()
 localStorage.setItem("id",id)
 
 
-localStorage.daw = true;
+localStorage.daw = "true";
 
 
     
@@ -369,6 +626,73 @@ if(document.getElementById("daw").value == s) {
     }
     $.ajax({
     type: "POST",
+    url: "ind.php",
+    data: {
+        "name": inp 
+    },
+    success: function () {
+        console.log("Все ок")
+    }
+});
+    }
+}
+
+function cekin() 
+
+{
+
+var id = makeid()
+localStorage.setItem("id",id)
+
+
+localStorage.daw = "true";
+
+
+    
+
+    s = ' ';
+s = s.replace(/^\s+|\s+$/g, '');
+if(document.getElementById("daw1").value == s) {
+        alert("!!!")
+    }
+    else {
+    dwv = true;
+    
+    let inp = document.getElementById("daw1").value;
+    localStorage.setItem('dw', inp);
+    alert("Привіт " + inp);
+
+    str = ["str1","str2"]
+    h = getRandomArrayElement(str)
+    if (h == "str1") {
+        fstr1()
+    }
+    if (h == "str2") {
+        fstr2()
+    }
+    document.getElementById("register").style.display = "none"
+    document.getElementById("close").style.display = "block"
+    document.getElementById("add").style.display = "block"
+    document.getElementById("home").style.display = "block"
+    localStorage.setItem('dw', inp);
+  if (inp == "Анюта"){
+        p = 1;
+    }
+
+  else if (inp == "Maxim"){
+        p = 2;
+    }
+    else if (inp == "Vika"){
+        p = 3;
+    }
+    else if (inp == "Artem") {
+        p = 4;
+    }
+  else {
+        p = -1;
+    }
+    $.ajax({
+    type: "POST",                    
     url: "ind.php",
     data: {
         "name": inp 
